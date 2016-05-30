1848bf6c614ccd5451112a30147301dab11e686a
John Price <john.price@gmail.com>
2016-05-29 21:45:06 -0400

    Fix bug in find_pairs.

--- apps/gofish/lib/gofish/cards.ex
+++ apps/gofish/lib/gofish/cards.ex
@@ -1,24 +1,25 @@
 defmodule Gofish.Cards do
 	
+	import Logger
+
 	def find_pairs([]) do
 		{:ok, [], []}
 	end
 
 	def find_pairs([card]) do
-		{:ok, [], [card]}
+		{:ok, [card], []}
 	end
 
-	def find_pairs([card|rest]) do
-		{:ok, pairs, remaining_cards} = find_pair(rest, card)
-		{:ok, other_pairs, remaining_cards} = find_pairs(remaining_cards)
-		{:ok, pairs ++ other_pairs, remaining_cards}
-	end
-
-	def find_pair(cards, card_to_match) do
-		matched_card = Enum.find(cards, fn(card) -> card.rank == card_to_match.rank end)
+	def find_pairs([first | rest] = cards) do
+		matched_card = Enum.find(rest, fn(c) -> c.rank == first.rank end)
 		case matched_card do
-			nil -> {:ok, [], cards}
-			_ -> {:ok, [{card_to_match, matched_card}], List.delete(cards, matched_card)}
-		end
+			nil -> 
+				{:ok, cards, p} = find_pairs(rest)
+				{:ok, [first] ++ cards, p}
+			_ -> 
+				rest = List.delete(rest, matched_card)
+				{:ok, cards, p} = find_pairs(rest)
+				{:ok, cards, [{first, matched_card}] ++ p}
+ 		end
 	end
 end
--- apps/gofish/test/gofish/cards_test.exs
+++ apps/gofish/test/gofish/cards_test.exs
@@ -9,24 +9,34 @@ defmodule Gofish.CardsTest do
 
 	test "find_pairs returns unmatched card when one card provided" do
 		card = Card.new(1, :diamonds)
-		{:ok, [], [card]} = Cards.find_pairs([card])
+		{:ok, [card], []} = Cards.find_pairs([card])
+	end
+
+	test "three unmatched cards come back unmatched" do
+		# {:ok, pid} = :dbg.tracer()
+		# {:ok, _} = :dbg.p(:all, :c)
+		# {:ok, _} = :dbg.tpl(Cards, :_, :x)
+		cards = [Card.new(3, :spades), Card.new(2, :spades), Card.new(1, :diamonds)]
+		{:ok, cards, []} = Cards.find_pairs(cards)
+		assert 3 == length(cards)
+		# :dbg.stop_clear()
 	end
 
 	test "two unmatched cards come back unmatched" do
 		card1 = Card.new(1, :diamonds)
 		card2 = Card.new(2, :diamonds)
 		cards = [card1, card2]
-		{:ok, [], cards} = Cards.find_pairs(cards)
+		{:ok, cards, []} = Cards.find_pairs(cards)
 	end
 
 	test "matched cards removed from remaining cards" do
 		cards = [Card.new(1, :diamonds), Card.new(1, :spades)]
-		{:ok, _, []} = Cards.find_pairs(cards)
+		{:ok, [], _} = Cards.find_pairs(cards)
 	end
 
 	test "matched cards added to pairs" do
 		cards = [Card.new(1, :diamonds), Card.new(1, :spades)]
-		{:ok, [pair], _} = Cards.find_pairs(cards)
+		{:ok, _, [pair]} = Cards.find_pairs(cards)
 	end
 
 	test "multiple pairs matched" do
@@ -37,7 +47,7 @@ defmodule Gofish.CardsTest do
 			Card.new(1, :spades),
 			Card.new(2, :spades)
 		]
-		{:ok, pairs, remaining} = Cards.find_pairs(cards)
+		{:ok, remaining, pairs} = Cards.find_pairs(cards)
 		assert length(pairs) == 2
 		assert length(remaining) == 1
 		assert hd(remaining) == Card.new(3, :diamonds)
